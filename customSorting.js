e29add9afb4512d1a5e69d1c2ddc1397e93087a9
Adib <adibalamir@hotmail.com>
2018-06-07 17:09:30 +0000

    sorted the array

---
+++ customSorting.js
@@ -0,0 +1,24 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b) {
+  var nameA = a.name; // ignore upper and lowercase
+  var nameB = b.name; // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  if (nameA === nameB) {
+    return b.age - a.age;
+  }
+  // names must be equal
+  return 0;
+});
+
+console.log(students);
